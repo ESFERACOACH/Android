82c83b7ff2e6f2fb9b9b5a89364465c9fff0434f
jbt <freddy@esferacoach.org>
2018-12-31 19:55:26 +0200

    Test Git

--- app/src/main/java/com/example/jbt/hellotest/MainActivity.kt
+++ app/src/main/java/com/example/jbt/hellotest/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         sample_text.text = stringFromJNI()
     }
 
-    /**
+    /**lkdjfhglkadjfhgsldkfjgh
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
